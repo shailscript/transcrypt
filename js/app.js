dc54385d946292a129c54388109c08d65e93a116
Tony Phuong Nguyen <nbphuong88@gmail.com>
2019-05-11 13:30:33 +0700

    Code refactor

--- js/app.js
+++ js/app.js
@@ -4,15 +4,17 @@ import credentials from './credentials.js'
 document.querySelector('form').addEventListener('submit', function(e) {
     e.preventDefault();
     let email = document.querySelector('input#search').value
-    console.log(email)
-    var studentInfo = credentials.studentsInfo.filter(stu => {
-        return stu.email = email
-    })
+    var studentInfo = credentials.studentsInfo.filter(stu => stu.student_email === email)
     const resultContainer = document.getElementById('results')
     if(studentInfo != undefined) {
         const renderResult = () => {
             resultContainer.innerHTML = ""
             resultContainer.style.display = "block"
+            if (studentInfo.length === 0) {
+                resultContainer.append(`Sorry, we couldn't find any results matching "${email}"`)
+                return
+            }
+
             studentInfo.forEach(item => {
                 let certificate = document.createElement('div')
                 let name = document.createElement('span')
@@ -25,7 +27,7 @@ document.querySelector('form').addEventListener('submit', function(e) {
 
                 certificate.className = 'certificate'
                 name.innerHTML = `<strong>Name :</strong> ${item.first_name} ${item.last_name}`
-                email.innerHTML = `<strong>Email :</strong> ${item.email}`
+                email.innerHTML = `<strong>Email :</strong> ${item.student_email}`
                 school.innerHTML = `<strong>School :</strong> ${item.school}`
                 depID.innerHTML = `<strong>Department ID :</strong> ${item.department_id}`
                 courseCode.innerHTML = `<strong>Course Code :</strong> ${item.Course_code}`
@@ -39,8 +41,3 @@ document.querySelector('form').addEventListener('submit', function(e) {
         renderResult()
     }
 })
-
-
-
-
-   

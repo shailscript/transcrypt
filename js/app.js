edd09f9b9f40a633ed6c55d8796d43c2c449cb8a
Mehrad Kavian <mehrad.kavian@gmail.com>
2019-05-10 22:07:28 -0400

    showing result

--- js/app.js
+++ js/app.js
@@ -5,11 +5,41 @@ document.querySelector('form').addEventListener('submit', function(e) {
     e.preventDefault();
     let email = document.querySelector('input#search').value
     console.log(email)
-    var studentInfo = credentials.studentsInfo.find(stu => {
-        return stu.name = email
+    var studentInfo = credentials.studentsInfo.filter(stu => {
+        return stu.email = email
     })
+    const resultContainer = document.getElementById('results')
     if(studentInfo != undefined) {
-        alert('found!')
+        const renderResult = () => {
+            resultContainer.innerHTML = ""
+            studentInfo.forEach(item => {
+                let certificate = document.createElement('div')
+                let name = document.createElement('span')
+                let email = document.createElement('span')
+                let school = document.createElement('span')
+                let depID = document.createElement('span')
+                let courseCode = document.createElement('span')
+                let status = document.createElement('span')
+                let grade = document.createElement('span')
+
+                certificate.className = 'certificate'
+                name.innerHTML = `<strong>Name :</strong> ${item.first_name} ${item.last_name}`
+                email.innerHTML = `<strong>Email :</strong> ${item.email}`
+                school.innerHTML = `<strong>School :</strong> ${item.school}`
+                depID.innerHTML = `<strong>Department ID :</strong> ${item.department_id}`
+                courseCode.innerHTML = `<strong>Course Code :</strong> ${item.Course_code}`
+                status.innerHTML = `<strong>Status :</strong> ${item.Status}`
+                grade.innerHTML = `<strong>Grade :</strong> ${item.Grade}`
+
+                certificate.append(name ,email, school, depID, courseCode, status, grade)
+                resultContainer.append(certificate)
+            })
+        }
+        renderResult()
     }
 })
+
+
+
+
    
